54638acb86adb8570573fe4bb024200ad3fbf2c5
d-broder <daniel_sendreti@hotmail.com>
2026-05-11 11:14:46 -0300

    ci: adicionar workflow do github actions e testes iniciais

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_math(self):
+        """Teste simples para garantir que o CI está rodando."""
+        self.assertEqual(1 + 1, 2)
